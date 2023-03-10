4609487c61ea90aa156a596fdc02d68ae6b8b812
AmirHosein <awir.software@gmail.com>
2023-03-10 22:48:25 +0330

    initialize bot for start

--- 01X01/main.py
+++ 01X01/main.py
@@ -1,2 +1,12 @@
 from decouple import config
 api_token = config('API_TOKEN')
+
+from telegram import Update
+from telegram.ext import CallbackContext, Updater
+
+updater = Updater(api_token)
+dispatcher = updater.dispatcher
+
+
+
+updater.start_polling()
