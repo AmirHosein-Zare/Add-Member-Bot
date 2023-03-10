b44a90ec07e10a636b40070ad782a612adf27c32
AmirHosein <awir.software@gmail.com>
2023-03-11 00:31:23 +0330

    finish develop

--- 01X01/main.py
+++ 01X01/main.py
@@ -10,12 +10,14 @@ dispatcher = updater.dispatcher
 
 def add_member(update: Update, context: CallbackContext):
     chat_id = update.message.chat.id
-    source_channel = '@playlist_dorhami'
-    destination_channel = '@testhunteradder'
-
-    source_chat_id = context.bot.get_chat(source_channel)
-    destination_chat_id = context.bot.get_chat(destination_channel)
+    source_channel = '@your source channel'
+    destination_channel = '@your destination channel'
+    # get channels id
+    source_chat_id = context.bot.get_chat(source_channel).id
+    destination_chat_id = context.bot.get_chat(destination_channel).id
+    # get all members from source channel
     members = context.bot.get_chat_members_count(source_chat_id)
+    # start add members to channel
     for member in range(members):
         user_id = context.bot.get_chat_member(source_chat_id, member).user.id
         try:
@@ -24,6 +26,7 @@ def add_member(update: Update, context: CallbackContext):
             pass
     context.bot.send_message(chat_id, "finish")
 
+# add handler => when user send /add then process start
 dispatcher.add_handler(CommandHandler('add', add_member))
 
 updater.start_polling()
